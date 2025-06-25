47d9d67111c7355c8bc136635fd6b68f04bade3f
action <xingdong1117@126.com>
2025-06-25 17:04:46 +0800

    feat:标签crud（枚举字段转换）

---
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/custom/config/WebMvcConfiguration.java
@@ -0,0 +1,28 @@
+package com.atguigu.lease.web.admin.custom.config;
+
+import com.atguigu.lease.web.admin.custom.converter.StringToBaseEnumConverterFactory;
+import com.atguigu.lease.web.admin.custom.converter.StringToItemTypeConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: action
+ * @create: 2025/6/25 15:20
+ **/
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private StringToItemTypeConverter stringToItemTypeConverter;
+
+    @Autowired
+    private StringToBaseEnumConverterFactory stringToBaseEnumConverterFactory;
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        //registry.addConverter(stringToBaseEnumConverterFactory);
+        registry.addConverterFactory(stringToBaseEnumConverterFactory);
+    }
+}
