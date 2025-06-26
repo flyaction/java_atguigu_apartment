c3ecd308dd5216062c5a0fdd0ba6275f190af0e3
action <xingdong1117@126.com>
2025-06-26 17:53:06 +0800

    新增minio的docker启动文件

---
+++ lease/common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,5 @@
+package com.atguigu.lease.common.minio;/**
+ * @author: action
+ * @create: 2025/6/26 16:54
+**/       public class MinioConfiguration {
+}
---
+++ lease/common/src/main/java/com/atguigu/lease/common/minio/MinioProperties.java
@@ -0,0 +1,5 @@
+package com.atguigu.lease.common.minio;/**
+ * @author: action
+ * @create: 2025/6/26 16:53
+**/       public class MinioProperties {
+}
