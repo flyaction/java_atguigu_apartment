39fddc574d01a2aea25d8110c9a3cefbc645230c
action <xingdong1117@126.com>
2025-06-27 17:35:03 +0800

    feat:公寓信息saveOrUpdate

--- lease/model/src/main/java/com/atguigu/lease/model/entity/ApartmentFeeValue.java
+++ lease/model/src/main/java/com/atguigu/lease/model/entity/ApartmentFeeValue.java
@@ -1,16 +1,13 @@
 package com.atguigu.lease.model.entity;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
 import lombok.Data;
 
 @Schema(description = "公寓&杂费关联表")
 @TableName(value = "apartment_fee_value")
 @Data
-@Builder
 public class ApartmentFeeValue extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
