9ce724d18c1aec7033220bd1081048a017fee6e2
action <xingdong1117@126.com>
2025-06-27 15:47:13 +0800

    feat:新增全局异常处理

---
+++ lease/common/src/main/java/com/atguigu/lease/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.atguigu.lease.common.exception;
+
+import com.atguigu.lease.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author: action
+ * @create: 2025/6/27 15:21
+ **/
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+}
