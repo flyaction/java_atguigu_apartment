ab519a03c28ada019ac825803f762a09f0aeb1a8
action <xingdong1117@126.com>
2025-06-27 14:55:48 +0800

    feat:上传图片到minio(配置字段映射+minio)

--- lease/common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
+++ lease/common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -1,5 +1,24 @@
-package com.atguigu.lease.common.minio;/**
+package com.atguigu.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
  * @author: action
  * @create: 2025/6/26 16:54
-**/       public class MinioConfiguration {
+ **/
+@Configuration
+@EnableConfigurationProperties(MinioProperties.class)
+public class MinioConfiguration {
+
+    @Autowired
+    private MinioProperties properties;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder().endpoint(properties.getEndpoint()).credentials(properties.getAccessKey(), properties.getSecretKey()).build();
+    }
 }
--- lease/common/src/main/java/com/atguigu/lease/common/minio/MinioProperties.java
+++ lease/common/src/main/java/com/atguigu/lease/common/minio/MinioProperties.java
@@ -1,5 +1,21 @@
-package com.atguigu.lease.common.minio;/**
+package com.atguigu.lease.common.minio;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
  * @author: action
  * @create: 2025/6/26 16:53
-**/       public class MinioProperties {
+ **/
+@ConfigurationProperties(prefix = "minio")
+@Data
+public class MinioProperties {
+
+    private String endpoint;
+
+    private String accessKey;
+
+    private String secretKey;
+
+    private String bucketName;
 }
