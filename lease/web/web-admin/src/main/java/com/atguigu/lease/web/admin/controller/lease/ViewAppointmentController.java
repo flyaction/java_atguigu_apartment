384396c1b7faba9edb3c07c41bd680058a458455
action <xingdong1117@126.com>
2025-06-30 16:37:18 +0800

    feat:预约看房管理(关联公寓信息，一对一association)

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/lease/ViewAppointmentController.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/lease/ViewAppointmentController.java
@@ -2,13 +2,20 @@ package com.atguigu.lease.web.admin.controller.lease;
 
 
 import com.atguigu.lease.common.result.Result;
+import com.atguigu.lease.model.entity.ViewAppointment;
 import com.atguigu.lease.model.enums.AppointmentStatus;
+import com.atguigu.lease.web.admin.service.ViewAppointmentService;
 import com.atguigu.lease.web.admin.vo.appointment.AppointmentQueryVo;
 import com.atguigu.lease.web.admin.vo.appointment.AppointmentVo;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 
 @Tag(name = "预约看房管理")
@@ -16,15 +23,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ViewAppointmentController {
 
+    @Autowired
+    private ViewAppointmentService service;
+
     @Operation(summary = "分页查询预约信息")
     @GetMapping("page")
     public Result<IPage<AppointmentVo>> page(@RequestParam long current, @RequestParam long size, AppointmentQueryVo queryVo) {
-        return Result.ok();
+
+        IPage<AppointmentVo> page = new Page<>(current, size);
+        IPage<AppointmentVo> list = service.pageAppointmentByQuery(page, queryVo);
+
+        return Result.ok(list);
     }
 
     @Operation(summary = "根据id更新预约状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam AppointmentStatus status) {
+
+        LambdaUpdateWrapper<ViewAppointment> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(ViewAppointment::getId, id)
+                .set(ViewAppointment::getAppointmentStatus, status)
+                .set(ViewAppointment::getUpdateTime, new Date(System.currentTimeMillis()));
+
+        service.update(wrapper);
+
         return Result.ok();
     }
 
