b8e857f394f3171d9b08f4767c54c7ef71ef8b52
action <xingdong1117@126.com>
2025-07-02 16:16:32 +0800

    feat:前台接口初始化

---
+++ lease/web/web-app/src/main/java/com/atguigu/lease/AppWebApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: action
+ * @create: 2025/7/2 16:04
+ **/
+@SpringBootApplication
+public class AppWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppWebApplication.class, args);
+    }
+}
