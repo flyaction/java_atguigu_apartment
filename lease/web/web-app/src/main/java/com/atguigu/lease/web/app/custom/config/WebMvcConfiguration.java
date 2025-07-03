939646b1d2571d63d0c8b8c34fbc62c2929361db
action <xingdong1117@126.com>
2025-07-03 15:05:13 +0800

    feat:登录注册+全局拦截器

---
+++ lease/web/web-app/src/main/java/com/atguigu/lease/web/app/custom/config/WebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.atguigu.lease.web.app.custom.config;
+
+import com.atguigu.lease.web.app.custom.interceptor.AuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: action
+ * @create: 2025/7/3 12:11
+ **/
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthenticationInterceptor authenticationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.authenticationInterceptor).addPathPatterns("/app/**").excludePathPatterns("/app/login/**");
+    }
+}
--- lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/LoginServiceImpl.java
+++ lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/LoginServiceImpl.java
@@ -3,9 +3,16 @@ package com.atguigu.lease.web.app.service.impl;
 import com.atguigu.lease.common.constant.RedisConstant;
 import com.atguigu.lease.common.exception.LeaseException;
 import com.atguigu.lease.common.result.ResultCodeEnum;
+import com.atguigu.lease.common.utils.JwtUtil;
 import com.atguigu.lease.common.utils.VerifyCodeUtil;
+import com.atguigu.lease.model.entity.UserInfo;
+import com.atguigu.lease.model.enums.BaseStatus;
 import com.atguigu.lease.web.app.service.LoginService;
 import com.atguigu.lease.web.app.service.SmsService;
+import com.atguigu.lease.web.app.service.UserInfoService;
+import com.atguigu.lease.web.app.vo.user.LoginVo;
+import com.atguigu.lease.web.app.vo.user.UserInfoVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -21,6 +28,9 @@ public class LoginServiceImpl implements LoginService {
 
     @Autowired
     private SmsService smsService;
+
+    @Autowired
+    private UserInfoService userInfoService;
 
 
     @Override
@@ -46,5 +56,54 @@ public class LoginServiceImpl implements LoginService {
         String verifyCode = VerifyCodeUtil.getVerifyCode(6);
         smsService.sendCode(phone, verifyCode);
         redisTemplate.opsForValue().set(key, verifyCode, RedisConstant.APP_LOGIN_CODE_TTL_SEC, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public String login(LoginVo loginVo) {
+        //1.判断手机号码和验证码是否为空
+        if (!StringUtils.hasText(loginVo.getPhone())) {
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_PHONE_EMPTY);
+        }
+
+        if (!StringUtils.hasText(loginVo.getCode())) {
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_CODE_EMPTY);
+        }
+
+        //2.校验验证码
+        String key = RedisConstant.APP_LOGIN_PREFIX + loginVo.getPhone();
+        String code = redisTemplate.opsForValue().get(key);
+        if (code == null) {
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_CODE_EXPIRED);
+        }
+
+        if (!code.equals(loginVo.getCode())) {
+            throw new LeaseException(ResultCodeEnum.APP_LOGIN_CODE_ERROR);
+        }
+
+        //3.判断用户是否存在,不存在则注册（创建用户）
+        LambdaQueryWrapper<UserInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(UserInfo::getPhone, loginVo.getPhone());
+        UserInfo userInfo = userInfoService.getOne(queryWrapper);
+        if (userInfo == null) {
+            userInfo = new UserInfo();
+            userInfo.setPhone(loginVo.getPhone());
+            userInfo.setStatus(BaseStatus.ENABLE);
+            userInfo.setNickname("用户-"+loginVo.getPhone().substring(6));
+            userInfoService.save(userInfo);
+        }
+
+        //4.判断用户是否被禁
+        if (userInfo.getStatus().equals(BaseStatus.DISABLE)) {
+            throw new LeaseException(ResultCodeEnum.APP_ACCOUNT_DISABLED_ERROR);
+        }
+
+        //5.创建并返回TOKEN
+        return JwtUtil.createToken(userInfo.getId(), loginVo.getPhone());
+    }
+
+    @Override
+    public UserInfoVo getUserInfoById(Long userId) {
+        UserInfo userInfo = userInfoService.getById(userId);
+        return new UserInfoVo(userInfo.getNickname(), userInfo.getAvatarUrl());
     }
 }
